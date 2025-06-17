220c3da417fb436f0f38095309cea1044d99bfa0
abdellatif <outegnitabdellatif@gmail.com>
2025-06-17 21:41:48 +0100

    i think this is it

--- controllers/AuthFunc.js
+++ controllers/AuthFunc.js
@@ -38,7 +38,7 @@ const signinFunc = async (req, res, next) => {
         httpOnly: true,
         secure: true, // Requires HTTPS
         sameSite: "none", // Crucial for cross-site cookies
-        domain: ".onrender.com", // Note the leading dot
+        domain: "https://ecofront.onrender.com", // Note the leading dot
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
       })
       .status(200)
@@ -60,7 +60,7 @@ const signinGoogleFunc = async (req, res, next) => {
           httpOnly: true,
           secure: true, // Requires HTTPS
           sameSite: "none", // Crucial for cross-site cookies
-          domain: ".onrender.com", // Note the leading dot
+          domain: "https://ecofront.onrender.com", // Note the leading dot
           maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
         })
         .status(200)
@@ -82,7 +82,7 @@ const signinGoogleFunc = async (req, res, next) => {
           httpOnly: true,
           secure: true, // Requires HTTPS
           sameSite: "none", // Crucial for cross-site cookies
-          domain: ".onrender.com", // Note the leading dot
+          domain: "https://ecofront.onrender.com", // Note the leading dot
           maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
         })
         .status(200)
